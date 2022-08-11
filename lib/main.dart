b7b962288a6e99c834215beb42ce468a4443177d
Alex Mora <aleemora@espol.edu.ec>
2022-08-10 23:41:17 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:g1amst/ui/exampleapp.dart';
+import 'package:g1amst/ui/routes.dart';
+
+import 'constants/colors.dart';
+
+void main() {
+  runApp(const ExampleApp());
+}
+
---
+++ lib/utils/info.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+List<Product> promos = [
+Product("assets/images/animal1.png", "Perrito Labrador", "Perrito Labrador", 115, 12, 1,
+Color(0XFFE3E2DC)),
+Product("assets/images/animal4.png", "Golden Retriever", "White-red Daily Shoes", 84, 12, 2,
+Color(0XFFE8E6CE)),
+Product("assets/images/animal3.png", "Labrador Pequeño", "Labrador Pequeño", 95, 12, 3,
+Color(0XFFD1E8CE)),
+Product("assets/images/animal2.png", "Perro mixto :)", "Perro mixto :)", 105, 12, 4,
+Color(0XFFF9DFB5)),
+];
